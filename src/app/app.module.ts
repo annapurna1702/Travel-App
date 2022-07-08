26b10b4b653d739a780800bec58def9342f57077
annapurna1702 <annapurnapadmaprema@gmail.com>
2022-07-08 06:52:40 +0530

    Travel UI developed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TravelregistrationComponent } from './travelregistration/travelregistration.component';
+import { SearchplacesComponent } from './searchplaces/searchplaces.component';
+import { ViewdestinationsComponent } from './viewdestinations/viewdestinations.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TravelregistrationComponent,
+    SearchplacesComponent,
+    ViewdestinationsComponent
   ],
   imports: [
     BrowserModule,
